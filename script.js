1cf14c730a7110ac48d92570e226d5aae6fdedac
Aadil-7 <aadilbhusnur007@gmail.com>
2022-09-08 20:46:27 +0530

    none professional commit message this is the code that im proud of and im sure i gonna reuse this code at some point in future

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+slideShow(slideIndex)
+
+const changeSlide = (n)=>{
+    slideShow(slideIndex += n)
+}
+
+const slidNave = (n)=>{
+    slideShow(slideIndex = n)}
+
+function slideShow(n) {
+    const slides = Array.from(document.getElementsByClassName("slide"))
+    const dots = Array.from(document.getElementsByClassName(`dot`))
+    if(n > slides.length) {slideIndex = 1 } 
+    n < 1 ? slideIndex = slides.length : slideIndex = slideIndex
+    slides.forEach(slide => slide.classList.remove(`activeSlide`))
+    dots.forEach(dot => dot.classList.remove(`activedot`))
+    
+    slides[slideIndex - 1].classList.add(`activeSlide`)
+    dots[slideIndex - 1].classList.add(`activedot`)
+}
+
+setInterval(()=>changeSlide(1), 5000)
